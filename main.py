9653123af34c3b60a309b867099dc16f3c34c534
PleasantPlant <yoshihiro.m.shimokawa@gmail.com>
2023-02-08 12:16:55 -0500

    split functions

--- main.py
+++ main.py
@@ -31,27 +31,12 @@ def run_flight_graph(start, stop):
         ("NYC", "CHI", 226),
         ("NYC", "MSP", 237),
     ]
-    cost = 0
-    i = 0
-    iterator = 0
-    readablePath = ""
     graph = Graph(edges)
     path = graph.dijsktra_shortest_path_airport(start, stop, name_dict)
     pathLen = len(path) - 1
-    while i < pathLen:
-        if (path[i] == edges[iterator][0] and path[i + 1] == edges[iterator][1]) or (
-            path[i] == edges[iterator][1] and path[i + 1] == edges[iterator][0]
-        ):
-            cost += edges[iterator][2]
-            i += 1
-        iterator += 1
-    for i in range(pathLen):
-        readablePath += "".join(name_dict[path[i]])
-        readablePath += "".join(" to ")
-        readablePath += "".join(name_dict[path[i + 1]])
-        readablePath += "".join(", ")
-
-    print(f"Your route is: {readablePath}")
+    cost = costCalculate(path, pathLen, edges)
+    fixedPath = makeReadable(pathLen, path, name_dict)
+    print(f"Your route is: {fixedPath}")
     print(f"The Total Cost is {cost} dollars.")
     match pathLen:
         case _ if pathLen > 2:
@@ -60,6 +45,38 @@ def run_flight_graph(start, stop):
             print("There is 1 layover on this trip.")
         case 1:
             print("There are no layovers on this trip.")
+    print("\n")
+
+
+def makeReadable(pathLen, path, name_dict):
+    readablePath = ""
+    for i in range(pathLen):
+        if i < pathLen - 1:
+            readablePath += "".join(name_dict[path[i]])
+            readablePath += "".join(" to ")
+            readablePath += "".join(name_dict[path[i + 1]])
+            readablePath += "".join(", ")
+        else:
+            readablePath += "".join("and ")
+            readablePath += "".join(name_dict[path[i]])
+            readablePath += "".join(" to ")
+            readablePath += "".join(name_dict[path[i + 1]])
+            readablePath += "".join(".")
+    return readablePath
+
+
+def costCalculate(path, pathLen, edges):
+    cost = 0
+    iterator = 0
+    i = 0
+    while i < pathLen:
+        if (path[i] == edges[iterator][0] and path[i + 1] == edges[iterator][1]) or (
+            path[i] == edges[iterator][1] and path[i + 1] == edges[iterator][0]
+        ):
+            cost += edges[iterator][2]
+            i += 1
+        iterator += 1
+    return cost
 
 
 def main():
